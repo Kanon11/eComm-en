708c45d52f911cf1174fa219128cd6a404e8e023
Kanon11 <kanonshet@yahoo.com>
2022-04-17 15:41:18 +0600

    header footer loginpage

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,21 @@
 @extends('master')
 @section('content')
-    <h1>Login Page</h1>
-    <button class="btn btn-primary">click me</button>
+    <div class="container login">
+        <div class="row">
+            <div class="col-sm-4 mx-auto">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
